316d94fe6c15f445a38c920cd3642a80258be018
Essam Bhatti <essambhatti@gmail.com>
2025-09-05 02:46:51 +0500

    Final commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -121,45 +121,27 @@ export const myProjects = [
 
 export const mySocials = [
   {
-    name: "WhatsApp",
-    href: "",
-    icon: "/assets/socials/whatsApp.svg",
-  },
-  {
     name: "Linkedin",
     href: "https://www.linkedin.com/in/essam-bhatti/",
     icon: "/assets/socials/linkedIn.svg",
   },
   {
-    name: "Instagram",
-    href: "https://www.instagram.com/ali.sanatidev/reels/",
-    icon: "/assets/socials/instagram.svg",
-  },
+    name: "Github",
+    href: "https://github.com/essambhatti",
+    icon: "assets/socials/github.svg"
+  }
+
 ];
 
 export const experiences = [
   {
-    title: "Software Developer",
-    job: "Security & Defense Projects",
-    date: "2021-2023",
+    title: "Mobile App Developer Intern",
+    job: "DigiLawyer",
+    date: "July 2025 - Present",
     contents: [
-      "Enhanced application security and developed new features, adhering to standards set by the Passive Defense Organization and National Cyberspace Center.",
-      "Designed and implemented intuitive map interfaces using MapsUI, enhancing user experience and enabling seamless interactive map integration.",
-      "Developed applications for industrial automation, leveraging C++ and the Fatek API for PLC communication.",
-      "Enhanced responsiveness and usability of applications using Windows Forms and WPF frameworks.",
-      "Executed XML to SVG conversions using X-DOM, ensuring dynamic and efficient data visualization.",
-    ],
-  },
-  {
-    title: "Back-End Developer",
-    job: "Car Manufacture",
-    date: "2023-2024",
-    contents: [
-      "Engineered systems for large-scale data ingestion and analysis, ensuring efficient data processing and storage.",
-      "Developed back-end systems enabling vehicle-to-cloud communication for telemetry, diagnostics, and remote control:",
-      "✅ Implemented secure APIs, following ISO 26262 automotive safety standards.",
-      "✅ Ensured data privacy for customers and partners through industry-compliant protocols.",
-      "✅ Delivered remote features like over-the-air updates, real-time tracking, and remote start capabilities.",
+       "Developed and optimized React Native components improving app performance and cross-platform consistency.",
+"Implemented core features (auth, navigation, data integration that improved usability for 500+ beta users.",
+"Collaborated with a small team using Agile practices, Git, and CI/CD workflows.",
     ],
   },
   {
@@ -167,8 +149,10 @@ export const experiences = [
     job: "Self-Employed",
     date: "2025-Present",
     contents: [
-      "Created a personal portfolio using Three.js, React, Vite, and WebAPI to showcase technical expertise.",
-      "Continuously enhancing technical skills and expanding expertise in modern web development and back-end technologies.",
+      "Building personal projects such as a portfolio (Three.js, React, Vite, WebAPI) to demonstrate technical skills.",
+    "Exploring full-stack development by experimenting with modern frameworks, databases, and cloud tools.",
+    "Focusing on AI integrations, real-time features, and deployment workflows to prepare for client projects.",
+    "Actively growing technical expertise and refining best practices in web and backend development."
     ],
   },
 ];
